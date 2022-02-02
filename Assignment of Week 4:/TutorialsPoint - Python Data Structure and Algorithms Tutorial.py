c9f20d7d8fdae42cba384f965befd4102a1047b3
jakkcanada <97357571+jakkcanada@users.noreply.github.com>
2022-02-01 19:22:18 -0700

    Add files via upload

---
+++ Assignment of Week 4:/TutorialsPoint - Python Data Structure and Algorithms Tutorial.py
@@ -0,0 +1,732 @@
+# Python - Linked Lists
+# Creation of Linked list
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+
+list1 = SLinkedList()
+list1.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+# Link first Node to second node
+list1.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
+
+# Traversing a Linked List
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+# Link first Node to second node
+list.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
+
+list.listprint()
+
+# Output
+Mon
+Tue
+Wed
+
+# Insertion in a Linked List
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+# Print the linked list
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+   def AtBegining(self,newdata):
+      NewNode = Node(newdata)
+
+# Update the new nodes next val to existing node
+   NewNode.nextval = self.headval
+   self.headval = NewNode
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.AtBegining("Sun")
+list.listprint()
+
+# Output
+Sun
+Mon
+Tue
+Wed
+
+# Inserting at the End
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+# Function to add newnode
+   def AtEnd(self, newdata):
+      NewNode = Node(newdata)
+      if self.headval is None:
+         self.headval = NewNode
+         return
+      laste = self.headval
+      while(laste.nextval):
+         laste = laste.nextval
+      laste.nextval=NewNode
+# Print the linked list
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.AtEnd("Thu")
+
+list.listprint()
+
+# Output
+Mon
+Tue
+Wed
+Thu
+
+# Inserting in between two Data Nodes
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+
+# Function to add node
+   def Inbetween(self,middle_node,newdata):
+      if middle_node is None:
+         print("The mentioned node is absent")
+         return
+
+      NewNode = Node(newdata)
+      NewNode.nextval = middle_node.nextval
+      middle_node.nextval = NewNode
+
+# Print the linked list
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Thu")
+
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.Inbetween(list.headval.nextval,"Fri")
+
+list.listprint()
+
+# Output
+Mon
+Tue
+Fri
+Thu
+
+# Removing an Item
+class Node:
+   def __init__(self, data=None):
+      self.data = data
+      self.next = None
+class SLinkedList:
+   def __init__(self):
+      self.head = None
+
+   def Atbegining(self, data_in):
+      NewNode = Node(data_in)
+      NewNode.next = self.head
+      self.head = NewNode
+
+# Function to remove node
+   def RemoveNode(self, Removekey):
+      HeadVal = self.head
+         
+      if (HeadVal is not None):
+         if (HeadVal.data == Removekey):
+            self.head = HeadVal.next
+            HeadVal = None
+            return
+      while (HeadVal is not None):
+         if HeadVal.data == Removekey:
+            break
+         prev = HeadVal
+         HeadVal = HeadVal.next
+
+      if (HeadVal == None):
+         return
+
+      prev.next = HeadVal.next
+         HeadVal = None
+
+   def LListprint(self):
+      printval = self.head
+      while (printval):
+         print(printval.data),
+         printval = printval.next
+
+llist = SLinkedList()
+llist.Atbegining("Mon")
+llist.Atbegining("Tue")
+llist.Atbegining("Wed")
+llist.Atbegining("Thu")
+llist.RemoveNode("Tue")
+llist.LListprint()
+
+# Output
+Thu
+Wed
+Mon
+
+# Python - Stack
+class Stack:
+   def __init__(self):
+      self.stack = []
+
+   def add(self, dataval):
+# Use list append method to add element
+      if dataval not in self.stack:
+         self.stack.append(dataval)
+         return True
+      else:
+         return False
+# Use peek to look at the top of the stack
+   def peek(self):     
+	   return self.stack[-1]
+
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+AStack.peek()
+print(AStack.peek())
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.peek())
+# Output
+Tue
+Thu
+# POP from a Stack
+class Stack:
+   def __init__(self):
+      self.stack = []
+
+   def add(self, dataval):
+# Use list append method to add element
+      if dataval not in self.stack:
+         self.stack.append(dataval)
+         return True
+      else:
+         return False
+        
+# Use list pop method to remove element
+   def remove(self):
+      if len(self.stack) <= 0:
+         return ("No element in the Stack")
+      else:
+         return self.stack.pop()
+
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.remove())
+print(AStack.remove())
+# Output
+Thu
+Wed
+# Python - Queue
+# Adding Elements
+class Queue:
+   def __init__(self):
+      self.queue = list()
+
+   def addtoq(self,dataval):
+# Insert method to add element
+   if dataval not in self.queue:
+      self.queue.insert(0,dataval)
+      return True
+   return False
+
+   def size(self):
+      return len(self.queue)
+
+TheQueue = Queue()
+TheQueue.addtoq("Mon")
+TheQueue.addtoq("Tue")
+TheQueue.addtoq("Wed")
+print(TheQueue.size())
+# Output
+3
+# Removing Element
+class Queue:
+   def __init__(self):
+      self.queue = list()
+
+   def addtoq(self,dataval):
+# Insert method to add element
+   if dataval not in self.queue:
+      self.queue.insert(0,dataval)
+      return True
+   return False
+# Pop method to remove element
+   def removefromq(self):
+      if len(self.queue)>0:
+         return self.queue.pop()
+      return ("No elements in Queue!")
+
+TheQueue = Queue()
+TheQueue.addtoq("Mon")
+TheQueue.addtoq("Tue")
+TheQueue.addtoq("Wed")
+print(TheQueue.removefromq())
+print(TheQueue.removefromq())
+# Output
+Mon
+Tue
+# Python - Dequeue
+import collections
+
+DoubleEnded = collections.deque(["Mon","Tue","Wed"])
+DoubleEnded.append("Thu")
+
+print ("Appended at right - ")
+print (DoubleEnded)
+
+DoubleEnded.appendleft("Sun")
+print ("Appended at right at left is - ")
+print (DoubleEnded)
+
+DoubleEnded.pop()
+print ("Deleting from right - ")
+print (DoubleEnded)
+
+DoubleEnded.popleft()
+print ("Deleting from left - ")
+print (DoubleEnded)
+# Output
+Appended at right - 
+deque(['Mon', 'Tue', 'Wed', 'Thu'])
+Appended at right at left is - 
+deque(['Sun', 'Mon', 'Tue', 'Wed', 'Thu'])
+Deleting from right - 
+deque(['Sun', 'Mon', 'Tue', 'Wed'])
+Deleting from left - 
+deque(['Mon', 'Tue', 'Wed'])
+# Python - Advanced Linked list
+# Creating Doubly linked list
+class Node:
+   def __init__(self, data):
+      self.data = data
+      self.next = None
+      self.prev = None
+
+class doubly_linked_list:
+   def __init__(self):
+      self.head = None
+
+# Adding data elements		
+   def push(self, NewVal):
+      NewNode = Node(NewVal)
+      NewNode.next = self.head
+      if self.head is not None:
+         self.head.prev = NewNode
+      self.head = NewNode
+
+# Print the Doubly Linked list		
+   def listprint(self, node):
+      while (node is not None):
+         print(node.data),
+         last = node
+         node = node.next
+
+dllist = doubly_linked_list()
+dllist.push(12)
+dllist.push(8)
+dllist.push(62)
+dllist.listprint(dllist.head)
+# Output
+62 8 12
+# Inserting into Doubly Linked List
+# Create the Node class
+class Node:
+   def __init__(self, data):
+      self.data = data
+      self.next = None
+      self.prev = None
+
+# Create the doubly linked list
+class doubly_linked_list:
+   def __init__(self):
+      self.head = None
+
+# Define the push method to add elements		
+   def push(self, NewVal):
+      NewNode = Node(NewVal)
+      NewNode.next = self.head
+      if self.head is not None:
+         self.head.prev = NewNode
+      self.head = NewNode
+
+# Define the insert method to insert the element		
+   def insert(self, prev_node, NewVal):
+      if prev_node is None:
+         return
+      NewNode = Node(NewVal)
+      NewNode.next = prev_node.next
+      prev_node.next = NewNode
+      NewNode.prev = prev_node
+      if NewNode.next is not None:
+         NewNode.next.prev = NewNode
+
+# Define the method to print the linked list 
+   def listprint(self, node):
+      while (node is not None):
+         print(node.data),
+         last = node
+         node = node.next
+
+dllist = doubly_linked_list()
+dllist.push(12)
+dllist.push(8)
+dllist.push(62)
+dllist.insert(dllist.head.next, 13)
+dllist.listprint(dllist.head)
+# Output
+62  8  13  12
+# Appending to a Doubly linked list
+# Create the node class
+class Node:
+   def __init__(self, data):
+      self.data = data
+      self.next = None
+      self.prev = None
+# Create the doubly linked list class
+class doubly_linked_list:
+   def __init__(self):
+      self.head = None
+
+# Define the push method to add elements at the begining
+   def push(self, NewVal):
+      NewNode = Node(NewVal)
+      NewNode.next = self.head
+      if self.head is not None:
+         self.head.prev = NewNode
+      self.head = NewNode
+
+# Define the append method to add elements at the end
+   def append(self, NewVal):
+      NewNode = Node(NewVal)
+      NewNode.next = None
+      if self.head is None:
+         NewNode.prev = None
+         self.head = NewNode
+         return
+      last = self.head
+      while (last.next is not None):
+         last = last.next
+      last.next = NewNode
+      NewNode.prev = last
+      return
+
+# Define the method to print
+   def listprint(self, node):
+      while (node is not None):
+         print(node.data),
+         last = node
+         node = node.next
+
+dllist = doubly_linked_list()
+dllist.push(12)
+dllist.append(9)
+dllist.push(8)
+dllist.push(62)
+dllist.append(45)
+dllist.listprint(dllist.head)
+# Output
+62 8 12 9 45
+# Python - Hash Table
+# Accessing Values in Dictionary
+# Declare a dictionary 
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+# Accessing the dictionary with its key
+print "dict['Name']: ", dict['Name']
+print "dict['Age']: ", dict['Age']
+# Output
+dict['Name']:  Zara
+dict['Age']:  7
+# Updating Dictionary
+# Declare a dictionary
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Age'] = 8; # update existing entry
+dict['School'] = "DPS School"; # Add new entry
+print "dict['Age']: ", dict['Age']
+print "dict['School']: ", dict['School']
+# Output
+dict['Age']:  8
+dict['School']:  DPS School
+# Delete Dictionary Elements
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+del dict['Name']; # remove entry with key 'Name'
+dict.clear();     # remove all entries in dict
+del dict ;        # delete entire dictionary
+
+print "dict['Age']: ", dict['Age']
+print "dict['School']: ", dict['School']
+# Output
+dict['Age']:
+Traceback (most recent call last):
+   File "test.py", line 8, in <module>
+      print "dict['Age']: ", dict['Age'];
+TypeError: 'type' object is unsubscriptable
+# Python - Binary Tree
+# Create Root
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
+   def PrintTree(self):
+      print(self.data)
+
+root = Node(10)
+root.PrintTree()
+# Output
+10
+# Inserting into a Tree
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
+
+   def insert(self, data):
+# Compare the new value with the parent node
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+            elif data > self.data:
+               if self.right is None:
+                  self.right = Node(data)
+               else:
+                  self.right.insert(data)
+      else:
+         self.data = data
+
+# Print the tree
+   def PrintTree(self):
+      if self.left:
+         self.left.PrintTree()
+      print( self.data),
+      if self.right:
+         self.right.PrintTree()
+
+# Use the insert method to add nodes
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+root.PrintTree()
+# Output
+3 6 12 14
+# Tree Traversal Algorithms
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
+# Insert Node
+   def insert(self, data):
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+         else data > self.data:
+            if self.right is None:
+               self.right = Node(data)
+            else:
+               self.right.insert(data)
+      else:
+         self.data = data
+# Print the Tree
+   def PrintTree(self):
+      if self.left:
+         self.left.PrintTree()
+      print( self.data),
+      if self.right:
+         self.right.PrintTree()
+# Inorder traversal
+# Left -> Root -> Right
+   def inorderTraversal(self, root):
+      res = []
+      if root:
+         res = self.inorderTraversal(root.left)
+         res.append(root.data)
+         res = res + self.inorderTraversal(root.right)
+      return res
+root = Node(27)
+root.insert(14)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+print(root.inorderTraversal(root))      
+# Output
+[10, 14, 19, 27, 31, 35, 42]
+# Pre-order Traversal
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
+# Insert Node
+   def insert(self, data):
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+         elif data > self.data:
+            if self.right is None:
+               self.right = Node(data)
+            else:
+               self.right.insert(data)
+         else:
+            self.data = data
+# Print the Tree
+   def PrintTree(self):
+      if self.left:
+         self.left.PrintTree()
+      print( self.data),
+      if self.right:
+         self.right.PrintTree()
+# Preorder traversal
+# Root -> Left ->Right
+   def PreorderTraversal(self, root):
+      res = []
+      if root:
+         res.append(root.data)
+         res = res + self.PreorderTraversal(root.left)
+         res = res + self.PreorderTraversal(root.right)
+      return res
+root = Node(27)
+root.insert(14)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+print(root.PreorderTraversal(root))
+# Output
+[27, 14, 10, 19, 35, 31, 42]
+# Post-order Traversal
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
+# Insert Node
+   def insert(self, data):
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+         else if data > self.data:
+            if self.right is None:
+               self.right = Node(data)
+            else:
+
+               self.right.insert(data)
+      else:
+         self.data = data
+# Print the Tree
+   def PrintTree(self):
+      if self.left:
+         self.left.PrintTree()
+print( self.data),
+if self.right:
+self.right.PrintTree()
+# Postorder traversal
+# Left ->Right -> Root
+def PostorderTraversal(self, root):
+res = []
+if root:
+res = self.PostorderTraversal(root.left)
+res = res + self.PostorderTraversal(root.right)
+res.append(root.data)
+return res
+root = Node(27)
+root.insert(14)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+print(root.PostorderTraversal(root))
+# Output
+[10, 19, 14, 31, 42, 35, 27]
